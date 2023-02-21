1a6ab77430d2968f6ad1d17676ab89ddf8bafe09
zachjohnson <johnsza5@oregonstate.edu>
2023-02-21 09:57:18 -0800

    Finished simple single linked list.

--- LinkedList.py
+++ LinkedList.py
@@ -31,6 +31,48 @@ class linked_list:
             elements.append(current_node.data)
         print(elements)
 
-my_list = linked_list()
+    def get(self, index):
+        if index >= self.length():
+            print("Error: 'Get' index out of range!")
+            return None
+        current_index = 0
+        current_node = self.head
+        while True:
+            current_node = current_node.next
+            if current_index == index: return current_node.data
+            current_index += 1
 
-my_list.display()
+    def erase(self, index):
+        if index >= self.length():
+            print("Error: 'Erase' Index out of range!")
+            return
+        current_index = 0
+        current_node = self.head
+        while True:
+            last_node = current_node
+            current_node = current_node.next
+            if current_index == index:
+                last_node.next = current_node.next
+                return
+            current_index += 1
+
+def main():
+    print("\n~~TESTS~~")
+    my_list = linked_list()
+
+    my_list.append(1)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(4)
+    print("\nThe full linked list is: ")
+    my_list.display()
+
+    my_list.erase(1)
+
+    print("\nThe appended linked list is: ")
+    my_list.display()
+
+    print("\nThe element at the second index is: " + str(my_list.get(2)))
+
+if __name__ == '__main__':
+    main()
